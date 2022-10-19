4d853fc6af6bd6c0e66cc0cffa880c3c202d471c
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2022-10-19 16:51:01 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comprasion
+        private int mov_count = 0; // number of data movements
+
+        // number of elements in array
+        private int n;
+        
+    }
+}
